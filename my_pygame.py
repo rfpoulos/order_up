e86723925105cfd16976b555ecf290ad0b800db7
Rachel Poulos <rachelpoulos@Rachels-MacBook-Air.local>
2018-02-08 14:00:22 -0500

    Added Ingredient and Bowl class to start making buttons

--- my_pygame.py
+++ my_pygame.py
@@ -7,25 +7,45 @@ screen = pygame.display.set_mode((canvas_width, canvas_height))
 clock = pygame.time.Clock()
 done = False
 
-ball_x = 100
-ball_y = 100
-ball_radius = 50
+class Ingredient:
+    def __init__(self, name, xcoor, ycoor):
+        self.name = name
+        self.xcoor = xcoor
+        self.ycoor = ycoor
+    def draw_ingredient(self):
+        pygame.draw.rect(screen, (175, 175, 175), (self.xcoor, self.ycoor, 75, 150), 0)
+
+class Bowl:
+    def __init__(self, ingredients = []):
+        self.ingredients = ingredients
+    def add_ingredient(self, ingredient):
+        self.ingredients.append(ingredient.name)
+
+lettuce = Ingredient("Lettuce", 50, 400)
+olives = Ingredient("Kalamata Olives", 150, 400)
+cheese = Ingredient("Blue Cheese", 250, 400)
+tomatoes = Ingredient("Cherry Tomatoes", 350, 400)
+croutons = Ingredient("Croutons", 450, 400)
+dressing = Ingredient("Dressing", 550, 400)
 while not done:
     for event in pygame.event.get():
-        print event #For debugging
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            ball_y -= 3
-        if pressed[pygame.K_DOWN]:
-            ball_y +=3
-        if pressed[pygame.K_LEFT]:
-            ball_x -=3
-        if pressed[pygame.K_RIGHT]:
-            ball_x +=3
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+        
+
         if event.type == pygame.QUIT:
             done = True
-        screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, (255, 0, 0), (ball_x, ball_y), ball_radius, 0)
+        screen.fill((225, 225, 225))
+        
+        #draw buttons
+        lettuce.draw_ingredient()
+        olives.draw_ingredient()
+        cheese.draw_ingredient()
+        tomatoes.draw_ingredient()
+        croutons.draw_ingredient()
+        dressing.draw_ingredient()
+
         pygame.display.update()
         clock.tick(60)
         
